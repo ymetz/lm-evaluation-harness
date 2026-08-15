feac28b18d1eb879008b7403f6d6e0c3912f2ab4
Yannick Metz <yametz@ethz.ch>
2026-08-15 13:33:23 -0700

    Update type hints in Filter class methods
    Avoids unit-test failes due to non instantiated constructors

--- lm_eval/api/filter.py
+++ lm_eval/api/filter.py
@@ -14,14 +14,13 @@ class Filter(ABC):
 
     """
 
-    @abstractmethod
     def __init__(self, **kwargs) -> None:
         """
         Can define custom behavior here, if an individual instantiation of a Filter class should have state.
         """
 
     @abstractmethod
-    def apply(self, resps: list | Iterable, docs: list[dict]) -> Iterable:
+    def apply(self, resps: Union[List, Iterable], docs: List[dict]) -> Iterable:
         """
         Defines the operation to perform on a list of the `inst.resps` properties of `Instance` objects.
         Should return the list of (filtered) response lists *in the same order as they were input*, e.g.
